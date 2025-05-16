06f8c2c237a97c907b240233426f776c4924cf30
sllado1 <sllado1@xtec.cat>
2025-05-16 09:34:46 +0200

    HTTPS nginx

--- certbot/init-letsencrypt.sh
+++ certbot/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(example.org www.example.org)
 rsa_key_size=4096
-data_path="../ngix/data/certbot"
+data_path="../nginx/data/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
