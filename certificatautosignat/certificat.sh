5e90194c5d7b2fb0e7e1e7b02693d8b98db4350e
sllado1 <sllado1@xtec.cat>
2025-05-16 09:53:19 +0200

    autosigned

---
+++ certificatautosignat/certificat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+domains=(catskills.cat gitea.catskills.cat)
+path="../nginx/data/certs"
+mkdir -p $path
+for domain in "${domains[@]}"; do
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout "$path/$domain.key" -out "$path/$domain.crt" -subj "/CN=$domain"
+done
